fe1a738be996e5e527ac4fd83d9717abbb71ac07
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-02-03 14:47:28 +0000

    enhance: add layout header + footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
+import Layout from "./components/layout/layoutContainer/Layout";
 import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
